bcaa043cddc5cfa8f64b33bc4ba7f49b704fffc1
Troy Christian <apostasy8@gmail.com>
2019-08-23 20:00:59 -0700

    Created Fragment, format main_activity xml, begin to format fragment xml, orient button to top of screen,

--- app/src/main/java/com/example/animated_rating/MainActivity.kt
+++ app/src/main/java/com/example/animated_rating/MainActivity.kt
@@ -2,20 +2,21 @@ package com.example.animated_rating
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.view.View
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentManagerNonConfig
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
-    var isFragmentLoaded = true
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var add = findViewById<Button>(R.id.button_add)
-        add.setOnClickListener({})
-
-        }
+override fun onCreate(savedInstanceState:Bundle?){
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    button_add.setOnClickListener{
+        val fragmentone = FragmentOne()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment, fragmentone).commit()
     }
-}
+}}
+
+
